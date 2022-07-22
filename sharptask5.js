15c91f9af0b584e8355b32b380584165bca2c85a
akashgits <akashbablu771@gmail.com>
2022-07-22 09:57:32 +0530

    task5

---
+++ sharptask5.js
@@ -0,0 +1,10 @@
+var grp=document.getElementsByTagName('li');
+console.log(grp)
+console.log(grp[1]);
+grp[1].textContent='hello'
+grp[1].style.font='bold';
+grp[2].style.backgroundColor='green'
+
+for(let i=0;i<grp.length;i++){
+    grp[i].style.fontWeight='bold'
+}

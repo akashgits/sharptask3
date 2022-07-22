45a45ea257c0605bf1c0cea632a6587162b3fb21
akashgits <akashbablu771@gmail.com>
2022-07-22 09:03:36 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,60 @@
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.title);
+
+// console.log(document.doctype)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.all)
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.links)
+// console.log(document.images)
+
+// console.log(document.getElementById('header-title'));
+
+
+
+const tit=document.getElementById('header-title')
+
+
+
+console.log(tit);
+console.log(tit.textContent);
+console.log(tit.innerText);
+
+tit.innerHTML='<h3>hello</h3>';
+const kit=document.getElementById('main-header')
+kit.style.borderBottom='solid 3px #000'
+
+const chang=document.getElementById('kit')
+chang.style.color='red'
+chang.style.font='bold'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

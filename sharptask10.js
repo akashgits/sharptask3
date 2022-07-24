96d5702c4bbb7544dfc4efd98fc5bed641ab464c
akashgits <akashbablu771@gmail.com>
2022-07-24 10:53:15 +0530

    task10

--- sharptask10.js
+++ sharptask10.js
@@ -1,1 +1,14 @@
-storage
+const form=document.getElementById('addForm');
+
+form.addEventListener('submit',toLocalstorage);
+
+
+
+function toLocalstorage(e){
+   
+    e.preventDefault();
+    var newItem =document.getElementById('item').value;
+    
+    localStorage.setItem('name',newItem);
+
+}
